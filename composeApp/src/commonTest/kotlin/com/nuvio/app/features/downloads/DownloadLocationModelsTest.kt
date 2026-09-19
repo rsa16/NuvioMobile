0d0de0ea18c15e25e46d1b0dada64001290dcef0
Rehan Ali <sipnet66@outlook.com>
2026-09-19 18:50:29 -0400

    fix(downloads): remove unused mimeTypeForFileName function and related tests

--- composeApp/src/commonTest/kotlin/com/nuvio/app/features/downloads/DownloadLocationModelsTest.kt
+++ composeApp/src/commonTest/kotlin/com/nuvio/app/features/downloads/DownloadLocationModelsTest.kt
@@ -35,29 +35,4 @@ class DownloadLocationModelsTest {
         assertEquals("\"ANDROID_SAF\"", json.encodeToString(DownloadLocationMode.ANDROID_SAF))
         assertEquals("\"IOS_FOLDER\"", json.encodeToString(DownloadLocationMode.IOS_FOLDER))
     }
-
-    @Test
-    fun mimeTypeMapsCommonVideoExtensions() {
-        assertEquals("video/mp4", mimeTypeForFileName("movie.mp4"))
-        assertEquals("video/mp4", mimeTypeForFileName("movie.MP4"))
-        assertEquals("video/mp4", mimeTypeForFileName("movie.m4v"))
-        assertEquals("video/x-matroska", mimeTypeForFileName("Show S01E01.mkv"))
-        assertEquals("video/webm", mimeTypeForFileName("clip.webm"))
-        assertEquals("video/x-msvideo", mimeTypeForFileName("clip.avi"))
-        assertEquals("video/quicktime", mimeTypeForFileName("clip.mov"))
-        assertEquals("video/mp2t", mimeTypeForFileName("clip.ts"))
-        assertEquals("video/x-flv", mimeTypeForFileName("clip.flv"))
-        assertEquals("video/x-ms-wmv", mimeTypeForFileName("clip.wmv"))
-        assertEquals("video/mpeg", mimeTypeForFileName("clip.mpeg"))
-        assertEquals("video/3gpp", mimeTypeForFileName("clip.3gp"))
-        assertEquals("video/ogg", mimeTypeForFileName("clip.ogv"))
-    }
-
-    @Test
-    fun mimeTypeFallsBackForUnknownOrMissingExtensions() {
-        assertEquals("application/octet-stream", mimeTypeForFileName("archive.zip"))
-        assertEquals("application/octet-stream", mimeTypeForFileName("no-extension"))
-        assertEquals("application/octet-stream", mimeTypeForFileName(""))
-        assertEquals("video/mp4", mimeTypeForFileName("clip.mp4?token=abc"))
-    }
 }
